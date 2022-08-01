20f51f057e92c764088c0e8e0537eb78074618b1
tamaron <tamaron1203@gmail.com>
2022-08-01 13:05:12 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/ErrorOr.h"
+#include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/raw_ostream.h"
+
+int main() { std::cout << "Hello" << std::endl; }
